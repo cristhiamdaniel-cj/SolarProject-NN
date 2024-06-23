de4d8574064f86f7129297bb8f9d88feb4b33e87
cristhiamdaniel <danielcampos.ingenieria@gmail.com>
2024-06-23 06:21:03 -0500

    datos main

--- data_split.py
+++ data_split.py
@@ -1,7 +1,7 @@
 # import json
 # import os
-# 
-# 
+#
+#
 # # Leer los modelos ya procesados
 # def read_processed_models(file_path):
 #     if not os.path.exists(file_path):
@@ -9,46 +9,46 @@
 #     with open(file_path, 'r') as f:
 #         processed_models = set(line.strip() for line in f)
 #     return processed_models
-# 
+#
 # # Leer el archivo JSON original y filtrar los modelos ya procesados
 # def filter_unprocessed_models(json_file, processed_models):
 #     with open(json_file, 'r') as f:
 #         panel_data = json.load(f)
 #     unprocessed_data = {key: value for key, value in panel_data.items() if key not in processed_models}
 #     return unprocessed_data
-# 
+#
 # # Dividir un diccionario en partes iguales
 # def split_dict_equally(input_dict, chunks=3):
 #     items = list(input_dict.items())
 #     size = len(items) // chunks
 #     return [dict(items[i * size:(i + 1) * size]) for i in range(chunks)]
-# 
+#
 # # Guardar las partes en archivos separados
 # def save_splits(splits):
 #     for i, part in enumerate(splits):
 #         with open(f'panelData_part_{i+1}.json', 'w') as f:
 #             json.dump(part, f, indent=4)
-# 
+#
 # def main():
 #     processed_models_file = 'processed_models.txt'
 #     json_file = 'panelData.json'
-# 
+#
 #     # Leer los modelos ya procesados
 #     processed_models = read_processed_models(processed_models_file)
-# 
+#
 #     # Filtrar el archivo JSON para excluir los modelos ya procesados
 #     unprocessed_data = filter_unprocessed_models(json_file, processed_models)
-# 
+#
 #     if not unprocessed_data:
 #         print("Todos los paneles han sido procesados.")
 #         return
-# 
+#
 #     # Dividir los datos en partes iguales
 #     split_data = split_dict_equally(unprocessed_data, chunks=4)
-# 
+#
 #     # Guardar las partes en archivos separados
 #     save_splits(split_data)
-# 
+#
 # if __name__ == "__main__":
 #     main()
 
@@ -151,14 +151,14 @@ def main():
     remaining_data = {panel: panel_data[panel] for panel in remaining_panels if panel in panel_data}
     print(f"Paneles restantes después de filtrar: {len(remaining_data)}")
 
-    # Definir las proporciones para dividir los datos
-    proportions = [0.5, 4 / 16, 3 / 16, 1 / 16]
-
-    # Dividir los datos en partes según las proporciones definidas
-    split_data = split_dict_proportionally(remaining_data, proportions)
-
-    # Guardar las partes en archivos separados e imprimir la cantidad de paneles
-    save_splits(split_data)
+    # # Definir las proporciones para dividir los datos
+    # proportions = [0.5, 4 / 16, 3 / 16, 1 / 16]
+    #
+    # # Dividir los datos en partes según las proporciones definidas
+    # split_data = split_dict_proportionally(remaining_data, proportions)
+    #
+    # # Guardar las partes en archivos separados e imprimir la cantidad de paneles
+    # save_splits(split_data)
 
 
 if __name__ == "__main__":
--- pv_model.py
+++ pv_model.py
@@ -417,7 +417,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # Configuración del logger para que guarde los logs en un archivo
-logging.basicConfig(filename='simulation.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='simulation_1.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def safe_exp(x):
     x_clipped = np.clip(x, None, 700)
@@ -537,8 +537,8 @@ def main():
     logger = logging.getLogger()
     logger.info('Leyendo datos del archivo JSON...')
 
-    json_file = 'extraccion/panelData_part_4.json'
-    processed_file = 'extraccion/processed_models_4.txt'
+    json_file = 'panelData_part_1.json'
+    processed_file = 'processed_models_1.txt'
     processed_models = read_processed_models(processed_file)
 
     model_names = get_model_names(json_file)
@@ -576,9 +576,9 @@ def main():
     data_df = pd.DataFrame(all_data, columns=columns)
 
     # Guardar los datos en el archivo CSV
-    header = not os.path.exists('extraccion/panel_simulation_data_4.csv')
-    data_df.to_csv('panel_simulation_data_4.csv', index=False, mode='a', header=header)
-    logger.info('Datos de simulación almacenados en panel_simulation_data_4.csv')
+    header = not os.path.exists('panel_simulation_data_1.csv')
+    data_df.to_csv('panel_simulation_data_1.csv', index=False, mode='a', header=header)
+    logger.info('Datos de simulación almacenados en panel_simulation_data_1.csv')
 
     # Actualizar el archivo de modelos procesados
     write_processed_models(processed_file, processed_models)
